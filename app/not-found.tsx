b1057ab4da865abbe15699c3dc4f187ea448415a
Arash Altafi <arashaltafi1377@gmail.com>
2024-04-18 20:19:01 +0430

    not found

---
+++ app/not-found.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { Metadata } from 'next';
+import React from 'react';
+import { Player } from "@lottiefiles/react-lottie-player";
+import Link from 'next/link';
+const notFound = "/lotties/404.json";
+
+const NotFound = () => {
+    return (
+        <div
+            className='select-none w-full h-[90vh] flex flex-col items-center justify-around px-16 py-8 overflow-hidden'>
+            <Player
+                className='w-1/2 h-auto'
+                src={notFound}
+                autoplay
+                loop
+                speed={1}
+            />
+
+            <h1 className='h1'>
+                Page Not Found !
+            </h1>
+
+            <Link href={'/'} className='btn btnPrimary'>
+                Back To Home
+            </Link>
+        </div>
+    )
+}
+
+export default NotFound
+
+export const metadata: Metadata = {
+    title: "Not Found | 404",
+    description: "This Page Noting Found!"
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
+        backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "torch":
+          'radial-gradient(525px at 50% 50%, #0EA5E922, transparent 50%)',
       },
     },
   },
