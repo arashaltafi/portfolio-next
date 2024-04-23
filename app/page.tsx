e8c2fb41b7a5d97cdb5e69d2c8bb7669cc9bc13c
Arash Altafi <arashaltafi1377@gmail.com>
2024-04-24 00:49:02 +0430

    a

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import About from "@/components/About";
 import Header from "@/components/Header";
 import Links from "@/components/Links";
 import Main from "@/components/Main";
-import ProgressBar from "@/components/ProgressBar";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 const Home = () => {
   return (
     <div className="md:snap-y md:snap-proximity w-full h-screen px-2 sm:px-4 md:px-8 xl:px-16 py-4 overflow-x-hidden">
-      <ProgressBar />
+      {/* <ProgressBar /> */}
       <Header />
       <Main />
       <Links />
