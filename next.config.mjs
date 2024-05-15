93a46b84a567cfd7902c8d7579cc42144ee77daf
arashaltafi <altafi.a@nahad.ir>
2024-05-15 14:35:27 +0330

    pwa service worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
-import withPWA from "next-pwa";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const pwa = withPWA({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    scope: '/app',
-    sw: 'service-worker.js',
-    fallbacks: {
-        document: '/offline',
-    },
+// https://ducanh-next-pwa.vercel.app/docs/next-pwa/getting-started
+// npm i @ducanh2912/next-pwa 
+// npm i -D webpack
+
+const pwa = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  scope: '/app',
+  sw: 'service-worker.js',
+  fallbacks: {
+      document: '/offline',
+  },
 });
 
 /** @type {import('next').NextConfig} */
