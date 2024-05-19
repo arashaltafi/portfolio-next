adba0bc06e3af585abc92e6f9e0d83eddc255a45
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-20 00:34:59 +0430

    update service worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 import withPWAInit from "@ducanh2912/next-pwa";
-import withPWA from "next-pwa";
 
+// way 1 (work success!)
 // https://ducanh-next-pwa.vercel.app/docs/next-pwa/getting-started
 // npm i @ducanh2912/next-pwa 
 // npm i -D webpack
+// npm i next-pwa
 
+// way 2
 // https://github.com/shadowwalker/next-pwa
 // https://www.npmjs.com/package/next-pwa
 // npm i next-pwa
