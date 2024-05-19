024e458eacdcb609edc6db0d363a24b088447653
Arash Altafi <arashaltafi1377@gmail.com>
2024-05-20 00:03:56 +0430

    update service worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,24 @@
 import withPWAInit from "@ducanh2912/next-pwa";
+import withPWA from "next-pwa";
 
 // https://ducanh-next-pwa.vercel.app/docs/next-pwa/getting-started
 // npm i @ducanh2912/next-pwa 
 // npm i -D webpack
 
+// https://github.com/shadowwalker/next-pwa
+// https://www.npmjs.com/package/next-pwa
+// npm i next-pwa
+
 const pwa = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  scope: '/app',
-  sw: 'service-worker.js',
-  fallbacks: {
-      document: '/offline',
-  },
+  skipWaiting: true,
+//   scope: '/app',
+//   sw: 'service-worker.js',
+//   fallbacks: {
+//       document: '/offline',
+//   },
 });
 
 /** @type {import('next').NextConfig} */
